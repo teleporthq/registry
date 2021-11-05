bf1298b67c6904142f375d4c130f8507902e0810
Jaya Krishna Namburu <namburu1995@gmail.com>
2021-11-05 12:05:21 +0530

    Revert "Switch to css-modules for zero-runtime for css (#11)" (#12)
    This reverts commit 6bd3cf8631c2ff915dbdc0632717228643fd0b75.

--- src/generator.ts
+++ src/generator.ts
@@ -1,47 +1,21 @@
-import { createStyleSheetPlugin } from "@teleporthq/teleport-plugin-react-styled-components";
+import reactComponentPlugin from "@teleporthq/teleport-plugin-react-base-component";
+import reactStyledComponentsPlugin, {
+  createStyleSheetPlugin,
+} from "@teleporthq/teleport-plugin-react-styled-components";
 import importStatementsPlugin from "@teleporthq/teleport-plugin-import-statements";
 import { createComponentGenerator } from "@teleporthq/teleport-component-generator";
-import { generateComponent } from "@teleporthq/teleport-code-generator";
-import { createStyleSheetPlugin as cssModulesStyleSheetPlugin } from "@teleporthq/teleport-plugin-css-modules";
-import {
-  ComponentGenerator,
-  ComponentType,
-  ComponentUIDL,
-  GeneratorOptions,
-  ReactStyleVariation,
-} from "@teleporthq/teleport-types";
+import { ReactMapping } from "@teleporthq/teleport-component-generator-react";
+import propTypesPlugin from '@teleporthq/teleport-plugin-jsx-proptypes'
 
-const styledComonentsGenerator = createComponentGenerator();
-styledComonentsGenerator.addPlugin(createStyleSheetPlugin());
-styledComonentsGenerator.addPlugin(importStatementsPlugin);
+const generator = createComponentGenerator();
+generator.addMapping(ReactMapping);
+generator.addPlugin(reactComponentPlugin);
+generator.addPlugin(propTypesPlugin)
+generator.addPlugin(reactStyledComponentsPlugin);
+generator.addPlugin(importStatementsPlugin);
 
-const cssModulesGenerator = createComponentGenerator();
-cssModulesGenerator.addPlugin(
-  cssModulesStyleSheetPlugin({ moduleExtension: true })
-);
+const styleSheetGenerator = createComponentGenerator();
+styleSheetGenerator.addPlugin(createStyleSheetPlugin());
+styleSheetGenerator.addPlugin(importStatementsPlugin);
 
-const styleSheetFactory = {
-  [ReactStyleVariation.StyledComponents]: styledComonentsGenerator,
-  [ReactStyleVariation.CSSModules]: cssModulesGenerator,
-};
-export class Generator {
-  compType: ComponentType = ComponentType.REACT;
-  styleType: ReactStyleVariation;
-
-  constructor(styleType?: ReactStyleVariation) {
-    this.styleType = styleType || ReactStyleVariation.CSSModules;
-  }
-
-  async component(uidl: ComponentUIDL, options: GeneratorOptions) {
-    return generateComponent(uidl, {
-      styleVariation: this.styleType,
-      componentType: ComponentType.REACT,
-      // @ts-ignore
-      componentGeneratorOptions: options,
-    });
-  }
-
-  get styleSheet() {
-    return styleSheetFactory[this.styleType];
-  }
-}
+export { generator, styleSheetGenerator };
